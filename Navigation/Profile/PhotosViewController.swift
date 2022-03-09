48c5bdca7f9b228dd408397e5b3e2a9effdcfa17
ustaschapouis <macbook@MacBook-Air-MacBook.local>
2022-03-09 23:26:15 +0300

    Done

--- Navigation/Profile/PhotosViewController.swift
+++ Navigation/Profile/PhotosViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import iOSIntPackage
 
 class PhotosViewController: UIViewController, UICollectionViewDelegate {
     
@@ -19,7 +20,10 @@ class PhotosViewController: UIViewController, UICollectionViewDelegate {
         return collectionView
     }()
     
-    let photos: Photos
+    var receivedImages = [UIImage]()
+    var imagePublisherFacade = ImagePublisherFacade()
+    
+    var photos: Photos
     
     init(photos: Photos) {
         self.photos = photos
@@ -34,13 +38,20 @@ class PhotosViewController: UIViewController, UICollectionViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        for i in Images.allCases {
+            print(i)
+            receivedImages.append(i.image(name: i))
+        }
+        
+//        let x = Images.checkmark
+//        receivedImages.append(x.image(name: .checkmark))
         view.addSubview(collectionView)
         collectionView.reloadData()
         navigationController?.setNavigationBarHidden(false, animated: true)
-//        navigationController?.navigationBar.isTranslucent = false
         navigationItem.title = "Photo Gallery"
         navigationController?.navigationBar.backgroundColor = .white
-
+        imagePublisherFacade.subscribe(self)
+        imagePublisherFacade.addImagesWithTimer(time: 1, repeat: 10, userImages: receivedImages)
     }
     
     override func viewWillLayoutSubviews() {
@@ -49,22 +60,43 @@ class PhotosViewController: UIViewController, UICollectionViewDelegate {
         collectionView.frame = CGRect(x: view.safeAreaInsets.left, y: view.safeAreaInsets.top, width: view.frame.width, height: view.frame.height - view.safeAreaInsets.top)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            imagePublisherFacade.subscribe(self)
+        
+        }
+        
+        override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+            
+            navigationController?.navigationBar.isHidden = true
+            imagePublisherFacade.removeSubscription(for: self)
+        }
+    
 }
+
 
 extension PhotosViewController: UICollectionViewDataSource {
     
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return photos.images.count
+        return receivedImages.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: PhotosCollectionViewCell.self), for: indexPath) as! PhotosCollectionViewCell
-        cell.feedPhoto.image = photos.images[indexPath.row]
-       
         
+        guard receivedImages.isEmpty == false else {
+            return cell
+        }
+      
+        cell.feedPhoto.image = receivedImages[indexPath.item]
+       
         return cell
     }
+    
+  
     
 }
 
@@ -86,4 +118,11 @@ extension PhotosViewController: UICollectionViewDelegateFlowLayout {
         return UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
     }
     
+}
+
+extension PhotosViewController: ImageLibrarySubscriber {
+    func receive(images: [UIImage]) {
+        receivedImages = images
+        collectionView.reloadData()
+    }
 }
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -15,20 +15,20 @@ class ProfileViewController: UIViewController {
     private var backgroundView:  UIView?
     private var crossButton: UIButton?
     let photos = Photos()
-    
+
     let userService: UserService
     let enteredUserName: String
-    
+
     init(userService: UserService, enteredUserName: String) {
         self.userService = userService
         self.enteredUserName = enteredUserName
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -38,7 +38,7 @@ class ProfileViewController: UIViewController {
 
     func setupView () {
         view.addSubview(tableView)
-        
+
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(PostTableViewCell.self, forCellReuseIdentifier: cellID)
         tableView.register(ProfileTableHeaderView.self, forHeaderFooterViewReuseIdentifier: "header")
@@ -46,13 +46,13 @@ class ProfileViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.backgroundColor = .systemGray5
-        
+
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.setNavigationBarHidden(true, animated: true)
     }
-    
+
     func setupConstraints() {
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: view.topAnchor),
@@ -61,6 +61,8 @@ class ProfileViewController: UIViewController {
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
         ])
     }
+
+
 }
 
 extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
@@ -70,7 +72,7 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
         }
         return Posts.content.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.section == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "Photos", for: indexPath) as! PhotosTableViewCell
@@ -78,10 +80,10 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
             cell.feedPhoto2.image = UIImage(named: photos.photos[indexPath.row + 1])
             cell.feedPhoto3.image = UIImage(named: photos.photos[indexPath.row + 2])
             cell.feedPhoto4.image = UIImage(named: photos.photos[indexPath.row + 3])
-            
+
             return cell
         }
-        
+
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID) as! PostTableViewCell
         let postCell = Posts.content[indexPath.row]
         cell.authorLabel.text = postCell.author
@@ -97,8 +99,8 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         if section == 0 {
             let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "header") as! ProfileTableHeaderView
-            
-            
+
+
         header.avatarImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(avatarResize)))
             header.avatarImage.isUserInteractionEnabled = true
 
@@ -106,14 +108,14 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
         } else {
             return nil
         }
-   
+
     }
 
     @objc func avatarResize(sender: UITapGestureRecognizer) {
-        
+
         self.view.layoutIfNeeded()
 
-        
+
         func setupAvatarImageView(){
             let imageView = sender.view as! UIImageView
             avatarImageView = UIImageView(image: imageView.image)
@@ -122,14 +124,14 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
             avatarImageView?.clipsToBounds = true
         }
         setupAvatarImageView()
-  
+
         func setupBackgroundView() {
             backgroundView = UIView(frame: UIScreen.main.bounds)
             backgroundView?.backgroundColor = UIColor.white
             backgroundView?.alpha = 0
         }
         setupBackgroundView()
-        
+
         func setupCrossButton(){
             crossButton = UIButton(type: .system)
             crossButton?.setBackgroundImage(UIImage(systemName: "cross"), for: .normal)
@@ -139,16 +141,17 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
             crossButton?.alpha = 0
         }
         setupCrossButton()
-        
-             
+
+
         self.view.addSubview(backgroundView ?? UIImageView())
         self.view.addSubview(avatarImageView ?? UIView())
         avatarImageView?.addSubview(crossButton ?? UIButton())
-        
-        
+
+
         self.tabBarController?.tabBar.isHidden = true
-    
-    
+
+        
+        
         UIView.animateKeyframes(withDuration: 0.5, delay: 0, options: [], animations: {
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1){
                 self.avatarImageView?.bounds.size.width = UIScreen.main.bounds.width
@@ -165,7 +168,7 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
             UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1){
                 self.backgroundView?.alpha = 0.7
             }
-            
+
         }, completion: {finished in
             UIView.animateKeyframes(withDuration: 0.3, delay: 0, options: [], animations: {
                 UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.2) {
@@ -175,21 +178,21 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
                             y: 0)
                     }
                 }
-                
+
                 UIView.addKeyframe(withRelativeStartTime: 0.3, relativeDuration: 0.8) {
                     self.crossButton?.alpha = 1
                 }
             })
         })
-
-        
+//
+//
         avatarImageView?.isUserInteractionEnabled = true
         crossButton?.isUserInteractionEnabled = true
-        
+//
         crossButton?.addTarget(self, action: #selector(self.reversViewAnimate), for: .touchUpInside)
         self.view.layoutIfNeeded()
     }
-    
+
     @objc func reversViewAnimate(){
         self.view.layoutIfNeeded()
         UIView.animateKeyframes(withDuration: 0.5, delay: 0, options: [], animations: {
@@ -202,7 +205,7 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
                 self.backgroundView?.alpha = 0
                 self.backgroundView = nil
             }
-            
+
             UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1){
                 self.avatarImageView?.alpha = 0
                 self.avatarImageView?.layer.cornerRadius = self.view.bounds.height / 2
@@ -216,7 +219,7 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
         })
         self.view.layoutIfNeeded()
     }
-           
+
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         if section == 0 {
         return 220
@@ -228,19 +231,19 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 2
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let photosVC = PhotosViewController(photos: photos)
         if indexPath.section == 0 {
         navigationController?.pushViewController(photosVC, animated: true)
     }
 }
-   
+
 }
-    
+
 extension ColorFilter {
     static func randomFilter() -> ColorFilter {
-        let randomInt = Int.random(in: 0...ColorFilter.allCases.count)
+        let randomInt = Int.random(in: 0...7)
         return self.allCases[randomInt]
     }
 }
